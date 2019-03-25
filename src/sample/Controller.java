445dc8998121a5732f8ae0a565a60062f6160230
studentvmautocad <hajkunic1@etf.unsa.ba>
2019-03-25 10:27:40 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import static sample.Main.sumaCifara;
+
+public class Controller {
+
+
+    public TextField tekst1;
+    public TextField tekst2;
+    public TextField rezultat;
+
+
+    public void ispisi(ActionEvent actionEvent) {
+        int a = Integer.parseInt(tekst1.getText());
+        int b = Integer.parseInt(tekst2.getText());
+        String result= "";
+        for(int i=a;i<=b;i++){
+            if(i%sumaCifara(i)==0)
+                result=result+" "+i;
+
+        }
+
+        rezultat.setText(result);
+
+    }
+}
